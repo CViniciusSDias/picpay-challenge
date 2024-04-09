f92e0245497478bc4eda7b28f816174c55a0ea85
Vinicius Dias <carlosv775@gmail.com>
2024-04-09 09:26:41 -0300

    Enabling SWOOLE_HOOK_ALL hook_flags

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'settings' => [
+        'hook_flags' => SWOOLE_HOOK_ALL,
+    ],
+];
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
